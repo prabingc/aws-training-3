66f1c14544c68b010ed3fc637bb92a0b4a01f623
Prabin Gautam <prabin.gautam@rackspace.com>
2025-06-11 13:26:19 +0000

    fixing fixing fixing:

--- vars/sharedVars.groovy
+++ vars/sharedVars.groovy
@@ -1,5 +1,5 @@
 return [
-  REGION: "us-east-2",
+  AWS_REGION: "us-east-2",
   CLUSTER_NAME: "prabin-eks",
   ECR_REGISTRY: "822875510783.dkr.ecr.us-east-2.amazonaws.com/pipeline-docker",
   ECR_REPO_NAME: 'pipeline-docker'
