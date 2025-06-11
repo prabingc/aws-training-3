6a57c68267bcd029a57b0ae244c6a3df6f4fe2ee
Prabin Gautam <prabin.gautam@rackspace.com>
2025-06-11 12:59:11 +0000

    added docker file

--- vars/sharedVars.groovy
+++ vars/sharedVars.groovy
@@ -1,5 +1,6 @@
 return [
   REGION: "us-east-2",
   CLUSTER_NAME: "prabin-eks",
-  ECR_REPO: "822875510783.dkr.ecr.us-east-2.amazonaws.com/pipeline-docker"
+  ECR_REPO: "822875510783.dkr.ecr.us-east-2.amazonaws.com/pipeline-docker",
+  ECR_REPO_NAME = 'pipeline-docker'
 ]
