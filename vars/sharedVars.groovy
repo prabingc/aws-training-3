227987c619465545d1f49033c2d5a6565e2ed7b4
Prabin Gautam <prabin.gautam@rackspace.com>
2025-06-11 13:29:54 +0000

    more typos

--- vars/sharedVars.groovy
+++ vars/sharedVars.groovy
@@ -1,6 +1,6 @@
 return [
   AWS_REGION: "us-east-2",
   CLUSTER_NAME: "prabin-eks",
-  ECR_REGISTRY: "822875510783.dkr.ecr.us-east-2.amazonaws.com/pipeline-docker",
+  ECR_REGISTRY: "822875510783.dkr.ecr.us-east-2.amazonaws.com",
   ECR_REPO_NAME: 'pipeline-docker'
 ]
